ccb2b9ac4deeb6d36739ef0b7d9a0a0a639ef3fc
Krestine H <krha0001@stud.kea.dk>
2024-05-11 17:45:04 +0200

    small tailwind update

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,31 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      "title-col": "#E6E6E6",
+      "font-col": "#CCCCCC",
+      "brand-one": { 50: "#FFC164", 100: "#AB7E33" },
+      "brand-two": "#CB6E00",
+      "brand-tree": "#68402A",
+      "brand-four": "#4B8372",
+      "brand-five": {
+        50: "#4FFF96",
+        100: "#2A8D52",
+      },
+      "grey-scale": {
+        "grey-one": "#FFFFFF",
+        "grey-two": "#E6E6E6",
+        "grey-tree": "#CCCCCC",
+        "grey-four": "#B3B3B3",
+        "grey-five": "#999999",
+        "grey-six": "#808080",
+        "grey-seven": "#666666",
+        "grey-eight": "#4D4D4D",
+        "grey-nine": "#333333",
+        "grey-ten": "#222222",
+        "grey-eleven": "#000000",
+      },
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
