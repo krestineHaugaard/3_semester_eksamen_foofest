d381182c74e950134cfe1dd16b235a3532525f94
Krestine H <krha0001@stud.kea.dk>
2024-05-22 15:42:48 +0200

    nogen ting skal fixes

--- src/app/booking_for/[id]/page.jsx
+++ src/app/booking_for/[id]/page.jsx
@@ -1,6 +1,7 @@
 import { getOrder } from "@/utils/orderdetailsapi";
 import TicketInformationForm from "@/components/TicketInformationForm";
 import CampingOptions from "@/components/CampingOptions";
+import { postTicketHolderInfo } from "@/utils/orderdetailsapi";
 
 export const dynamic = "force-dynamic";
 
@@ -15,14 +16,89 @@ export default async function BookingInformation({ params }) {
 
   console.log(cleanNumber);
 
+  async function submit(formdata) {
+    "use server";
+
+    formdata.map(
+      (form) => form.get("first_name"),
+      form.get("last_name"),
+      form.get("email"),
+      form.get("phone"),
+      form.get("birthday"),
+      form.get("address"),
+      form.get("postal_code"),
+      form.get("city"),
+      form.get(`ticket_choice`),
+      orderid
+    );
+  }
+  // se om det kan laves og gemmes og postes som array til den opruindelige data base i stedet
+
   return (
     <section>
       <CampingOptions />
-      {Array(cleanNumber)
-        .fill(true)
-        .map((index) => (
-          <TicketInformationForm key={index + 1} />
-        ))}
+      <form action={submit}>
+        {Array(cleanNumber)
+          .fill()
+          .map((item, index) => {
+            return (
+              <div key={index}>
+                <h2>Ticket {index + 1}</h2>
+                <div>
+                  <div>
+                    <label htmlFor={`ticket_reguler${index}`}>Regular</label>
+                    <input
+                      id={`ticket_reguler${index}`}
+                      type="radio"
+                      value="reguler"
+                      name={"ticket_choice" + { index }}
+                    />
+                    <label htmlFor={`ticket_vip${index}`}>VIP</label>
+                    <input
+                      id={`ticket_vip${index}`}
+                      type="radio"
+                      value="vip"
+                      name={"ticket_choice" + { index }}
+                    />
+                  </div>
+                  <label htmlFor={`first_name${index}`}>First name</label>
+                  <input
+                    id={`first_name${index}`}
+                    type="text"
+                    name="first_name"
+                  />
+                  <label htmlFor={`last_name${index}`}>Last name</label>
+                  <input
+                    id={`last_name${index}`}
+                    type="text"
+                    name="last_name"
+                  />
+                  <label htmlFor={`email${index}`}>E-mail</label>
+                  <input id={`email${index}`} type="e-mail" name="email" />
+                  <label htmlFor={`phone_number${index}`}>Phone number</label>
+                  <input id={`phone_number${index}`} type="text" name="phone" />
+                  <label htmlFor={`date_of_birth${index}`}>Date of birth</label>
+                  <input
+                    id={`date_of_birth${index}`}
+                    type="date"
+                    name="birthday"
+                  />
+                  <label htmlFor={`address${index}`}>Address</label>
+                  <input id={`address${index}`} type="text" name="address" />
+                  <label htmlFor={`postal_code${index}`}>Postal code</label>
+                  <input
+                    id={`postal_code${index}`}
+                    type="text"
+                    name="postal_code"
+                  />
+                  <label htmlFor={`city${index}`}>City</label>
+                  <input id={`city${index}`} type="text" name="city" />
+                </div>
+              </div>
+            );
+          })}
+        <button type="submit">Submit</button>
+      </form>
     </section>
   );
 }
--- src/utils/orderdetailsapi.js
+++ src/utils/orderdetailsapi.js
@@ -42,3 +42,49 @@ export async function getOrder(param) {
 
   return response.json();
 }
+
+export async function postTicketHolderInfo(
+  first_name,
+  last_name,
+  email,
+  tlf_number,
+  birthday,
+  address_line,
+  postal_code,
+  city,
+  ticket_chosen,
+  order_id
+) {
+  const headersList = {
+    "Content-Type": "application/json",
+    apikey:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl1aXNvZ2p3dm50Zm94dWRvb2xuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA3NTE5NDUsImV4cCI6MjAyNjMyNzk0NX0.A4lJrT15zWBvm6Zm7nXbtq01CUOrjNct49-JgtQboeg",
+    Prefer: "return=representation",
+  };
+
+  const bodyContent = JSON.stringify({
+    first_name: first_name,
+    last_name: last_name,
+    email: email,
+    tlf_number: tlf_number,
+    birthday: birthday,
+    address_line: address_line,
+    postal_code: postal_code,
+    city: city,
+    ticket_chosen: ticket_chosen,
+    order_id: order_id,
+  });
+
+  const response = await fetch(
+    "https://yuisogjwvntfoxudooln.supabase.co/rest/v1/ticket_holder_information",
+    {
+      method: "POST",
+      body: bodyContent,
+      headers: headersList,
+    }
+  );
+
+  const data = [response.json()];
+
+  console.log(data);
+}
